677fe3ab21260606d16088aaa86de3f4085690e2
Galvin Eka Nurullah <114245994+galvinnurullah@users.noreply.github.com>
2022-12-04 09:03:31 +0700

    Create Integrasi_Metode-Trapesium.py

---
+++ Galvin Eka Nurullah/Integrasi/Integrasi_Metode-Trapesium.py
@@ -0,0 +1,31 @@
+# Metode Trapesium
+
+# Mendefinisikan fungsi untuk dilakukan integrasi
+def f(x):
+    return 1/(1 + x**2)
+
+# Implementasi metode trapesium
+def trapesium(x0,xn,n):
+    # perhitungan ukuran langkah
+    h = (xn - x0) / n
+    
+    # Menemukan penjumlahan
+    integrasi = f(x0) + f(xn)
+    
+    for i in range(1,n):
+        k = x0 + i*h
+        integrasi = integrasi + 2 * f(k)
+    
+    # Menemukan nilai akhir dari integrasi
+    integrasi = integrasi * h/2
+    
+    return integrasi
+    
+# bagian imput
+batasBawah = float(input("Masukan batas bawah dari integrasi: "))
+batasAtas = float(input("Masukan batas atas dari integrasi: "))
+subInterval = int(input("Masukan angka untuk sub intervals: "))
+
+# Memanggil fungsi metode trapesium dan menampilkan hasil
+hasil = trapesium(batasBawah, batasAtas, subInterval)
+print("Hasil integrasi adalah : %0.6f" % (hasil) )

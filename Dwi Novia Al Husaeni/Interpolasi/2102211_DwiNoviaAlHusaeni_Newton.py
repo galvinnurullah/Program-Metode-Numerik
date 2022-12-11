e710dcbb092fca256b7cac315432d2a4ffb64ad3
Dwi Novia Al Husaeni <117613488+DwiNoviaAlHusaeni@users.noreply.github.com>
2022-12-11 20:24:27 +0700

    Add files via upload

---
+++ Dwi Novia Al Husaeni/Interpolasi/2102211_DwiNoviaAlHusaeni_Newton.py
@@ -0,0 +1,37 @@
+# untuk import library
+import numpy as np
+
+
+banyakData = int(input('Masukan jumlah inputan data x dan y: '))
+nilaiX = np.zeros((banyakData))
+nilaiY = np.zeros((banyakData, banyakData))
+
+
+print('Masukan data x dan y: ')
+for i in range(banyakData):
+    nilaiX[i] = float(input('nilaiX(' + str(i) + ') = '))
+    nilaiY[i][0] = float(input('nilaiY(' + str(i) + ') = '))
+    
+
+xinput = input('Masukkan nilai x yang ingin diketahui: ')
+xinput = float(xinput)
+
+for i in range(1, banyakData):
+    for j in range(0, banyakData-i):
+        nilaiY[j][i] = (nilaiY[j + 1, i - 1] - nilaiY[j, i - 1])/(nilaiX[j+i] - x[j])
+
+print('\n============================================TABEL SELISIH - TERBAGI==============================================');
+for i in range(0, banyakData):
+    print('%0.2f' % (x[i]), end='')
+    for j in range(0, banyakData - i):
+        print('\t\t%0.6f' % (nilaiY[i][j]), end='')
+    print()
+print('=================================================================================================================');
+
+p = nilaiY[0,0]
+for i in range (1, banyakData):
+    a = nilaiY[0,i]
+    for j in range(0, i):
+        a = a * (xinput-x[j])
+    p = p+a
+print('Nilai hampiran dari data di atas adalah %0.6f' % p)
